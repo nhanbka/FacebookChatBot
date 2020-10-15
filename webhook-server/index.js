d53463de9954d17a8ae5cf8de8c8eba3a832ebc3
NhanBKA <nhanletrong999@gmail.com>
2020-10-15 10:01:49 +0700

    first commit

---
+++ webhook-server/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
